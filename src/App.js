c40605af9c4c08be5f8bcc806f7dcb8ad7e9ee9f
Yashwini <yashwinibadjatya@gmail.com>
2019-10-18 00:49:11 +0530

    add Header in all pages

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/HomePage';
 import { Route } from 'react-router-dom';
+import Header from './components/header/Header';
 import ShopPage from './pages/shop/shop';
 
 const App = () => {
   return (
     <div>
+      <Header />
       <Route exact path="/" component={HomePage} />
       <Route path="/shop" component={ShopPage} />
       {/* <HomePage /> */}
