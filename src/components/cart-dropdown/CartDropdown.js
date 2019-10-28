05add27bdde36d63af128f1947ea85add7c254fe
Yashwini Badjatya <yashwinibadjatya@gmail.com>
2019-10-28 16:44:46 +0530

    add reselector for memoization

--- src/components/cart-dropdown/CartDropdown.js
+++ src/components/cart-dropdown/CartDropdown.js
@@ -3,6 +3,7 @@ import './CartDropdown.scss';
 import { connect } from 'react-redux';
 import CustomButton from '../../components/custom-button/CustomButton';
 import CartItem from '../cart-item/CartItem';
+import { selectCartItems } from '../../redux/cart/cart.selector';
 const CartDropdown = ({ cartItems }) => {
   return (
     <div className="cart-dropdown">
@@ -15,7 +16,7 @@ const CartDropdown = ({ cartItems }) => {
     </div>
   );
 };
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems
+const mapStateToProps = state => ({
+  cartItems: selectCartItems(state)
 });
 export default connect(mapStateToProps)(CartDropdown);
